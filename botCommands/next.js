c98566ea5b551db6f86ed2e8def4010914073f39
anhhung294 <hoanganhhung29042004@gmail.com>
2022-06-10 19:50:37 +0700

    fix next

--- botCommands/next.js
+++ botCommands/next.js
@@ -1,8 +1,9 @@
-const guildDBModel = require('../models/guild.js');
+const guildModel = require('../models/guild.js');
 
 module.exports = {
     name: 'next',
     async execute(msg){
-
+        const guildDB = await guildModel.findOne({guildId: msg.guildId});
+        
     }
 }
